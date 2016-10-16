1d229a562df28dafbbad888fb48ee766daf8efbe
Robert Sandor <robertsandor@Roberts-MacBook-Pro-2.local>
2016-10-16 00:14:17 -0700

    Created DoubleLinkedList file; created classes and added variables

---
+++ DoublyLinkedListImplementation.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace DataStructure
+{
+  class Node
+  {
+    private Object data;
+    private Node next;
+    private Node previous;
+  }
+
+  class DoublyLinkedList
+  {
+    private Node head;
+
+
+  }
+}
+
+static void Main(string[] args)
+{
+  
+  Console.ReadKey();
+}

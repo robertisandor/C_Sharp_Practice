238dc511745b081c9087264430224d32a0fcc8d5
tofugrace <robert.i.sandor@gmail.com>
2016-10-16 00:31:20 -0700

    Modified Nodes & DoublyLinkedList to be generic and compiled

--- DoublyLinkedListImplementation.cs
+++ DoublyLinkedListImplementation.cs
@@ -1,24 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 namespace DataStructure
 {
-  class Node
+  public class Node<T>
   {
-    private Object data;
-    private Node next;
-    private Node previous;
+    private T data;
+    private Node<T> next;
+    private Node<T> previous;
   }
 
-  class DoublyLinkedList
+  public class DoublyLinkedList<T>
   {
-    private Node head;
+    private Node<T> head;
+    private int size;
 
-
+    public DoublyLinkedList()
+    {
+      size = 0;
+      head = null;
+    }
+    public DoublyLinkedList(int givenSize)
+    {
+      size = givenSize;
+      head = null;
+    }
   }
-}
 
-static void Main(string[] args)
-{
-  
-  Console.ReadKey();
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DoublyLinkedList<int> list = new DoublyLinkedList<int>(5);
+      Console.ReadKey();
+    }
+  }
 }

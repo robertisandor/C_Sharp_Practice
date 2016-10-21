ecc0ab999a8cc08b4d25c8461101d33d00549262
tofugrace <robert.i.sandor@gmail.com>
2016-10-20 23:32:34 -0700

    Configuring first LinkedList unit test

---
+++ UnitTest.cs
@@ -0,0 +1,23 @@
+using System;
+using NUnit.Framework;
+
+namespace PracticeTest
+{
+  class PracticeClass
+  {
+    public PracticeClass()
+    {
+
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+
+    public string helloWorld()
+    {
+      return "Hello World!";
+    }
+  }
+}

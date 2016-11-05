4962783a7f2b49916f047226d740667e22e8e9cb
Robert Sandor <robertsandor@Roberts-MacBook-Pro-2.local>
2016-11-04 20:26:50 -0700

    Added and ran first test

--- LinkedListImplementation-UnitTest.cs
+++ LinkedListImplementation-UnitTest.cs
@@ -1,10 +1,16 @@
 using System;
+using NUnit.Framework;
 
 namespace SimpleDataStructures
 {
   [TestFixture]
   public class LinkedListTestClass
   {
-    
+    [Test]
+    public void ConstructorTest()
+    {
+      LinkedList testList = new LinkedList();
+      Assert.Equals(testList.Count, 0);
+    }
   }
 }

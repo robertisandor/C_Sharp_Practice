120c9223923d225a3f01a3250468170b4ba6dbf7
Robert Sandor <robertsandor@Roberts-MacBook-Pro-2.local>
2016-11-04 20:04:15 -0700

    Added variables and constructors

---
+++ LinkedListImplementation.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Node
+{
+  private Object data;
+  private Node next;
+
+  public Node()
+  {
+
+  }
+}
+
+public class LinkedList
+{
+  private Node head;
+
+  public LinkedList()
+  {
+    
+  }
+}

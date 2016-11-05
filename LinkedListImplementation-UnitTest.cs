3d70dfc40cd2a16c7f9d541cc8270803aea4257e
Robert Sandor <robertsandor@Roberts-MacBook-Pro-2.local>
2016-11-04 23:26:11 -0700

    Revised method and test for AddFirst to include if head wasn't empty

--- LinkedListImplementation-UnitTest.cs
+++ LinkedListImplementation-UnitTest.cs
@@ -21,6 +21,15 @@ namespace SimpleDataStructures
       testList.AddFirst(10);
       Assert.AreEqual(testList.Count, 1);
       Assert.AreEqual(10, testList.Start.data);
+      testList.AddFirst(20);
+      Assert.AreEqual(testList.Count, 2);
+      Assert.AreEqual(20, testList.Start.data);
+    }
+
+    [Test]
+    public void PrintAllNodesTest()
+    {
+      // Figure out how to test console output
     }
   }
 }

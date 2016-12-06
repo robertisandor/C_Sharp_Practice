faa4adbc7ac3294f2f2698aca516af2bf94e70a1
Robert Sandor <robertsandor@Roberts-MacBook-Pro-2.local>
2016-12-06 01:20:54 -0800

    Edited & renamed tests so the constructor test runs first & the AddToEnd test is ignored

--- DoublyLinkedListImplementation-UnitTest.cs
+++ DoublyLinkedListImplementation-UnitTest.cs
@@ -9,28 +9,37 @@ namespace SimpleDataStructures
         DoublyLinkedList<int> testList = new DoublyLinkedList<int>();
 
         [Test]
-        public void ConstructorTest()
+        public void A_ConstructorTest()
         {
             Assert.AreEqual(0, testList.Size);
             Assert.AreEqual(null, testList.Head);
         }
 
-        public void AddToFrontTest()
+        [Test]
+        public void B_AddToFrontTest()
         {
             testList.AddToFront(11);
             Assert.AreEqual(1, testList.Size);
             Assert.AreNotEqual(null, testList.Head);
             Assert.AreEqual(11, testList.Head.data);
+            testList.AddToFront(22);
+            Assert.AreEqual(2, testList.Size);
+            Assert.AreEqual(22, testList.Head.data);
+            Assert.AreEqual(11, testList.Head.next.data);
         }
 
+        [Test]
+        [Ignore("Ignoring this test for now.")]
         public void AddToEndTest()
         {
             testList.AddToEnd(55);
-            Assert.AreEqual(2, testList.Size);
+            Assert.AreEqual(1, testList.Size);
             Assert.AreNotEqual(null, testList.Head);
-            Assert.AreEqual(11, testList.Head.data);
-            Assert.AreEqual(testList.Head, testList.Head.next.previous);
-            Assert.AreEqual(55, testList.Head.next.data);
+            Assert.AreEqual(55, testList.Head.data);
+            testList.AddToEnd(66);
+            Assert.AreEqual(2, testList.Size);
+            Assert.AreNotEqual(null, testList.Head.next);
+            Assert.AreEqual(66, testList.Head.next.data);
         }
     }
 }
